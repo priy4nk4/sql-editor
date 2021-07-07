dd7b2bd55dcb786a27d8e8bd6224906ee6ee951c
Priyanka Das <guddy.priyanka.2013@gmail.com>
2021-07-07 13:53:40 +0530

    remove: extra code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { AceEditorComponent } from 'ng2-ace-editor';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import 'brace';
-import 'brace/mode/sql';
-import "brace/theme/chrome";
-import "brace/mode/yaml";
-// import 'ace-builds/webpack-resolver';
-import { DataService } from './data.service';
-
-import 'ace-builds/src-noconflict/mode-java';
-import 'ace-builds/src-noconflict/ext-language_tools';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -20,35 +9,7 @@ export class AppComponent {
 
 
 
-  constructor(private modalservice: NgbModal, private data: DataService){}
-
-  @ViewChild('editor') editor!:  AceEditorComponent;
- 
-  options:any = {maxLines: 1000, printMargin: false};
-  text:any = '';
- 
-
-  ngAfterViewInit() {
-      this.editor.setTheme("eclipse");
-
-      this.editor.getEditor().setOptions({
-          enableBasicAutocompletion: true
-      });
-
-      this.editor.getEditor().commands.addCommand({
-          name: "showOtherCompletions",
-          bindKey: "Ctrl-.",
-          exec: function (editor: any) {
-            console.log(editor);
-          }
-      })
-      this.editor.setOptions({
-        enableBasicAutocompletion: true,
-        
-        enableSnippets: true,
-        enableLiveAutocompletion: false
-    });
-  }
+  constructor(){}
 
 }
 
